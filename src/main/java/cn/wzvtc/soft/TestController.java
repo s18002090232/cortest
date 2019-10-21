f64bb01a599cf983cdd4cd4af8d7b4d4edbdcf70
admin <2826275232@qq.com>
2019-10-21 09:13:55 +0800

    init commit

--- src/main/java/cn/wzvtc/soft/TestController.java
+++ src/main/java/cn/wzvtc/soft/TestController.java
@@ -1,6 +1,9 @@
 package cn.wzvtc.soft;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -9,11 +12,11 @@ import java.util.Map;
 @RestController("/" )
 public class TestController {
 
-    @RequestMapping(value="data.json",method={RequestMethod.GET})
+    @RequestMapping(value="/data.json",method={RequestMethod.GET})
     public Map<String,String> bookById(){
         Map<String,String> resultMap=new HashMap<>();
-        resultMap.put("myname","田所浩二");
-        resultMap.put("mynumber","114514");
+        resultMap.put("myname","xwh");
+        resultMap.put("mynumber","18002090232");
         return resultMap;
     }
 }
